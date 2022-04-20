4b3075da3290af11bebc63c781f5242da0a9f952
gurami98 <chilindrishviligurami@gmail.com>
2022-04-20 22:35:35 +0400

    useEffect finished and conditional rendering

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -3,10 +3,37 @@ import React, { useState } from 'react';
 // ternary operator
 
 const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
+  const [text, setText] = useState('');
+  const [isError, setIsError] = useState(false);
+  const firstValue = text || 'hello world';
+  const secondValue = text && 'hello world';
+  // console.log(firstValue);
+  // console.log(secondValue);
 
-  return <h2>short circuit</h2>;
+  return (
+      <>
+        {/* <h1>{firstValue}</h1>
+        <h1>value : {secondValue}</h1> */}
+        {/* {if(){console.log('hello world')}} */}
+        <h1>{text || 'john doe'}</h1>
+        {text && <h1>Hello World</h1>}
+        {!text && <h1>Hello World</h1>}
+
+        <button className="btn" onClick={() => setIsError(!isError)}>
+          toggle error
+        </button>
+
+        {isError && <h1>Error...</h1>}
+
+        {
+          isError ? (
+            <h1>Error with ternary</h1>
+          ) : (
+            <h1>No Error with ternary</h1>
+          )
+        }
+      </>
+  );
 };
 
 export default ShortCircuit;
